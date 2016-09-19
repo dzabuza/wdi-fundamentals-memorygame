793b589428850076f8d8279b814961c596f29d47
Kevin Arora <kevinarora@Kevins-MBP.home>
2016-09-19 04:26:43 -0400

    js file wont create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,42 @@
-var cardOne = "Queen", cardTwo = "Queen", cardThree = "King", cardFour ="King";
+var cards = ['queen','queen','king','king'];
+var cardsinplay = [];
+
+// think the problem is somewhere here. I am unable to make the cards generate
+function createBoard(){
+
+var board = document.getElementsByClassName('board')[0];
+
+for(var i=0;i<cards.length;i++){
+var cardElement = document.createElement('div');
+cardElement.classname = 'card';
+cardElement.innerHTML = cards[i];
+
+cardElement.addEventListener('click',istwocards)
+board.appendChild(cardElement);
+}
+}
+
+function istwocards(){
+cardsinplay.push(this.innerHTML);
+
+if(cardsinplay.length === 2){
+	ismatch(cardsinplay);
+
+	cardsinplay = [];
+}
+
+
+}
+
+
+function ismatch(cards){
+	if(cards[0]===cards[1]){
+
+		alert("its a match!");
+	}
+	else{
+		alert("try again");
+	}
+createboard();
+}
+
